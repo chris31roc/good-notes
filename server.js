a4331cc86e3dc55752cee2f0076f4715e185e517
chris31roc <chris31roc@yahoo.com>
2021-02-04 23:27:08 -0500

    all files now perfected

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+// Creating an "express" server
+const app = express();
+
+// Sets an initial port
+const PORT = process.env.PORT || 8080;
+
+// Sets Express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
